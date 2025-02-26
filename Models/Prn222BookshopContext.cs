f6a4d9960752a5f05fddcb5715430a9ff66a5fbb
vietsdasasd <luuviet504@gmail.com>
2025-02-26 14:02:57 +0700

    Thêm CRUD book

--- Models/Prn222BookshopContext.cs
+++ Models/Prn222BookshopContext.cs
@@ -32,8 +32,13 @@ public partial class Prn222BookshopContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PRN222_bookshop; Integrated Security=True; Connect Timeout=30;TrustServerCertificate=True;");
+    {
+        var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfigurationRoot configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("Default"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Services/Book/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Services.Book
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Prn222BookshopContext _context;
+
+        public CategoryRepository(Prn222BookshopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
