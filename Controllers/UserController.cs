01efa2ea71a44ad466d3e25df2c02e567ca9ef40
ThanhNT9112 <thanhnthe160345@fpt.edu.vn>
2025-02-26 17:43:36 +0700

    save

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,10 +7,12 @@ namespace BookStore.Controllers
     public class UserController : Controller
     {
         private readonly Prn222BookshopContext prn222BookshopContext;
+        private readonly ILogger<UserController> _logger;
 
-        public UserController(Prn222BookshopContext prn222BookshopContext)
+        public UserController(Prn222BookshopContext prn222BookshopContext, ILogger<UserController> logger)
         {
             this.prn222BookshopContext = prn222BookshopContext;
+            _logger = logger;
         }
 
         public IActionResult UserProfile(int id)
@@ -25,7 +27,7 @@ namespace BookStore.Controllers
 
 		public IActionResult EditProfile(int id)
 		{
-			var user = prn222BookshopContext.Users.FirstOrDefault(u => u.UserId == id);
+			var user = prn222BookshopContext.Users.Find(id);
 			if (user == null)
 			{
 				return NotFound();
@@ -36,20 +38,18 @@ namespace BookStore.Controllers
 		
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public IActionResult EditProfile(User model)
+		public async Task<IActionResult> EditProfile(User model)
 		{
-			if (!ModelState.IsValid)
-			{
-				return View(model);
-			}
+			
 
-			var user = prn222BookshopContext.Users.Find(model.UserId);
+			var user = await prn222BookshopContext.Users.FindAsync(model.UserId);
 			if (user == null)
 			{
-				return NotFound();
+                
+                return NotFound();
 			}
-
-			user.Username = model.Username;
+           
+            user.Username = model.Username;
 			user.Email = model.Email;
 			user.Address = model.Address;
 			user.Status = model.Status;
@@ -57,11 +57,12 @@ namespace BookStore.Controllers
             try
             {
                 prn222BookshopContext.Users.Update(user);
-                prn222BookshopContext.SaveChanges(); 
+                await prn222BookshopContext.SaveChangesAsync();
                 return RedirectToAction("UserProfile", new { id = user.UserId });
             }
             catch (Exception ex)
             {
+               
                 ModelState.AddModelError("", "Error saving data: " + ex.Message);
                 return View(model);
             }
