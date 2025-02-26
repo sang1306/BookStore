684c67386ae527b0fc52fc5346333214eb7990b4
ThanhNT9112 <thanhnthe160345@fpt.edu.vn>
2025-02-26 16:58:57 +0700

    edit profile

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Controllers
 {
@@ -22,5 +23,50 @@ namespace BookStore.Controllers
             return View(user);
         }
 
-    }
+		public IActionResult EditProfile(int id)
+		{
+			var user = prn222BookshopContext.Users.FirstOrDefault(u => u.UserId == id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			return View(user);
+		}
+
+		
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult EditProfile(User model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var user = prn222BookshopContext.Users.Find(model.UserId);
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			user.Username = model.Username;
+			user.Email = model.Email;
+			user.Address = model.Address;
+			user.Status = model.Status;
+
+            try
+            {
+                prn222BookshopContext.Users.Update(user);
+                prn222BookshopContext.SaveChanges(); 
+                return RedirectToAction("UserProfile", new { id = user.UserId });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error saving data: " + ex.Message);
+                return View(model);
+            }
+
+		}
+
+	}
 }
