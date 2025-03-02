975c33a7ff66497b76222b0f875c1a1817073334
khamhung0910 <139756011+khamhung0910@users.noreply.github.com>
2025-03-02 13:36:11 +0700

    Add files via upload

---
+++ Page/User/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+
+
+
+namespace BookStore.Pages.User
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Prn222BookshopContext _context;
+
+        public IndexModel(Prn222BookshopContext context)
+        {
+
+            _context = context;
+        }
+
+        public List<Models.User> Users { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            Users = await _context.Users.ToListAsync();
+        }
+    }
+}
