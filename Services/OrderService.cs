13b2d84ed5b3b324f477ac1b35941ba132b6dfb6
huyBach1609 <bachlh03@gmail.com>
2025-03-04 10:46:20 +0700

    view card

---
+++ Services/OrderService.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Models;
+
+namespace BookStore.Services
+{
+    public class OrderService
+    {
+        private readonly Prn222BookshopContext _context;
+        public OrderService(Prn222BookshopContext context)
+        {
+            _context = context; 
+        }
+        public List<BookStore.Models.Book> GetBooksByIds(List<int> listId) {
+            return _context.Books.Where(b => listId.Contains(b.BookId)).ToList();
+        }
+    }
+}
